89727785eaac48f20c9e296c5ec8597f666a1bd6
isarodrigues2024 <i.pratesrodrigues@stud.unibas.ch>
2026-03-17 18:05:20 +0100

    Done

---
+++ HypothesisTesting.py
@@ -0,0 +1,12 @@
+# Hypothesis Testing
+## Use mean value here
+# Hypothesis Testing - Statistics Spring 2026
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+# Your code here
+print("Ready for hypothesis testing!")
+
+x = [1 2 3 50 40 65 700 40 500 2 3 8];
+np.mean(x);
